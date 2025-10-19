7131ef4c40307afbe9562c11ab646846e020b5a3
Nathan Haynes-Magyar <njhmagyar@gmail.com>
2025-10-19 17:07:15 -0400

    Switch S3 to bucket policy instead of ACLs for public access

--- portfolio_chat/settings/production.py
+++ portfolio_chat/settings/production.py
@@ -43,7 +43,7 @@ AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default='')
 AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
